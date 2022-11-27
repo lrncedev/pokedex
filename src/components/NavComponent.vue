102685d6b9f5966c1e6855e841c2518f8826ea4f
Laurence Cadag <laurencecads1@gmail.com>
2022-11-27 12:27:46 +0800

    SCSS working

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav-comp">
+    <h1 class="app-title">Test</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "NavComponent",
+};
+</script>
+<style lang="scss">
+.nav-comp {
+  padding: $md;
+
+  .app-title {
+    color: $accent-text;
+  }
+}
+</style>
